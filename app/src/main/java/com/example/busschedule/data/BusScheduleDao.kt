308bdfc9a6ee3ca11c0b9da9768491850c0ee4ed
pabloSMelian <pablosanmel05@gmail.com>
2023-11-05 10:22:36 +0000

    Intento 1

---
+++ app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
@@ -0,0 +1,27 @@
+package com.example.busschedule.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import androidx.room.Delete
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BusScheduleDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(busSchedule: BusSchedule)
+    @Update
+    suspend fun update(busSchedule: BusSchedule)
+    @Delete
+    suspend fun delete(busSchedule: BusSchedule)
+    @Query("SELECT * from busSchedules WHERE id = :id")
+    fun getItem(id: Int): Flow<BusSchedule>
+    @Query("SELECT * from busschedules ORDER BY arrivalTime ASC")
+    fun getAll(): Flow<List<BusSchedule>>
+    @Query("SELECT * from busschedules ORDER BY stopName ASC")
+    fun getAllByName(): Flow<List<BusSchedule>>
+    @Query("SELECT * from busschedules WHERE stopName = :name")
+    fun getByName(name: String): Flow<List<BusSchedule>>
+}
